7bb944de60f4b0fd8000a9d5df66a5bd141b1bfe
Ryan McCann <ryanjohnmccann@gmail.com>
2022-03-19 10:04:12 -0400

    Added broadcast broken functionality

--- prod_cons_MT.c
+++ prod_cons_MT.c
@@ -5,14 +5,15 @@
 #include <stdlib.h>
 
 // TODO: Signals
+// TODO: May have to use two buffer locks?
 
 extern struct monitor m1;
 
 void init_monitor() {
-    int i;
-    for (i = 0; i < m1.b_size; i++) {
-        m1.shared_buffer[i] = -1;
-    }
+//    int i;
+//    for (i = 0; i < m1.b_size; i++) {
+//        m1.shared_buffer[i] = -1;
+//    }
     m1.producer_pos = 0;
     m1.nums_produced = m1.n_producers * (m1.b_size * 2);
     m1.divide = m1.nums_produced / m1.n_consumers;
@@ -31,7 +32,7 @@ void *Producer(void *t) {
         int rand_int = (rand() % 10) + 1;
         pthread_mutex_lock(&m1.buffer_lock);
         // Entry is empty
-        if (m1.shared_buffer[m1.producer_pos] == -1) {
+        if (m1.shared_buffer[m1.producer_pos] == 0) {
             printf("P%ld: Writing %d to position %d\n", tid, rand_int, m1.producer_pos);
             m1.shared_buffer[m1.producer_pos] = rand_int;
             if (m1.producer_pos == (m1.b_size - 1)) {
@@ -46,6 +47,7 @@ void *Producer(void *t) {
             pthread_cond_wait(&m1.full, &m1.buffer_lock);
         }
         pthread_mutex_unlock(&m1.buffer_lock);
+        pthread_cond_broadcast(&m1.empty);
     }
 
     printf("P%ld: Exiting\n", tid);
@@ -53,8 +55,7 @@ void *Producer(void *t) {
 }
 
 void *Consumer(void *t) {
-    long tid, start_ind, end_ind, i;
-
+    long tid, start_ind, end_ind, i, tmp;
     tid = (long) t;
 
     // TODO: All threads will get one and the last will be skipped
@@ -72,8 +73,26 @@ void *Consumer(void *t) {
         end_ind = start_ind + m1.divide;
     }
 
-    printf("C%ld: Consuming %ld values\n", tid, (end_ind - start_ind));
-    for (i = start_ind; i < end_ind; i++) {
-
+    long will_consume = end_ind - start_ind;
+    printf("C%ld: Consuming %ld values\n", tid, will_consume);
+    while (will_consume > 0) {
+        for (i = start_ind; i < end_ind; i++) {
+            pthread_mutex_lock(&m1.buffer_lock);
+            // Position is empty
+            if (m1.shared_buffer[i] == 0) {
+                continue;
+            } else {
+                will_consume -= 1;
+                tmp = m1.shared_buffer[i];
+                printf("C%ld Reading %ld from position %ld\n", tid, tmp, i);
+                m1.shared_buffer[i] = 0;
+            }
+            pthread_mutex_unlock(&m1.buffer_lock);
+            pthread_cond_broadcast(&m1.full);
+        }
+        if (will_consume > 0) {
+            printf("C%ld Blocked due to empty buffer\n", tid);
+            pthread_cond_wait(&m1.empty, &m1.buffer_lock);
+        }
     }
 }
